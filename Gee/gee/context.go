f7a064e36ff6b729ebeb1cb08cfaf6f5c2b6593e
whiteeeLi <1521803664@qq.com>
2024-06-25 23:38:42 +0800

    day2 finish

---
+++ Gee/gee/context.go
@@ -0,0 +1,88 @@
+package gee
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// H 这里的 H 会被作为输入扔到生成json的函数中作为参数
+// 在main.go 中可以直接使用gee.H{xxx}直接生成输入
+// 如果没有 H 就得使用 map[string]interface{} 做类型声明
+type H map[string]interface{}
+
+type Context struct {
+	// origin objects
+	Writer http.ResponseWriter
+	Req    *http.Request
+	// request info
+	Path   string
+	Method string
+	// response info
+	StatusCode int
+}
+
+// 需要一个创建Context的函数，值得注意的是这里是私有的函数，仅在context包中使用
+// 对应 gee.go中的 c := newContext(w, req)
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	//返回一个Context对象的地址
+	return &Context{
+		Writer: w,
+		Req:    req,
+		Path:   req.URL.Path,
+		Method: req.Method,
+	}
+}
+
+// Query 提供对url中参数的提取功能，返回用户查询参数对应的值
+func (c *Context) Query(key string) string {
+	//下面这玩意想要写出来需要查http包的相关函数，
+	//相当于是封装了一下，不用用户自己打这个代码
+	return c.Req.URL.Query().Get(key)
+}
+
+// PostForm 提供查询Post提交表单的内容的功能
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+// SetHeader 用于设置请求头
+func (c *Context) SetHeader(key, value string) {
+	c.Writer.Header().Set(key, value)
+}
+
+// Status 设置状态码
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
+
+// String 往响应中写入string
+func (c *Context) String(code int, format string, values ...interface{}) {
+	c.SetHeader("Content-Type", "text/plain")
+	c.Status(code)
+	c.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+// Data 用于直接向响应体中写入二进制数据
+func (c *Context) Data(code int, data []byte) {
+	c.Status(code)
+	c.Writer.Write(data)
+}
+
+// JSON 向响应体中写入json数据
+func (c *Context) JSON(code int, obj interface{}) {
+	c.SetHeader("Content-Type", "application/json")
+	c.Status(code)
+	encoder := json.NewEncoder(c.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(c.Writer, err.Error(), 500)
+	}
+}
+
+// HTML 向响应体写入html
+func (c *Context) HTML(code int, html string) {
+	c.SetHeader("Content-Type", "text/html")
+	c.Status(code)
+	c.Writer.Write([]byte(html))
+}
--- Gee/gee/gee.go
+++ Gee/gee/gee.go
@@ -1,69 +1,44 @@
 package gee
 
-import (
-	"fmt"
-	"net/http"
-)
+import "net/http"
 
-// HandlerFunc
-// 我们需要一个类型来储存handler
-// 其实这玩意就是http包里面对HandlerFunc的定义，这里不知道维之门没有直接引用http中的类型
-// 毕竟我们还是要把HandlerFunc传给http的函数
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+// HandlerFunc defines the request handler used by gee
+// 这里是最先发生变化的地方，类型改了
+type HandlerFunc func(*Context)
 
-// Engineer 用于储存请求与handler之间的映射关系
-type Engineer struct {
-	route map[string]HandlerFunc
+// Engine implement the interface of ServeHTTP
+// 因为将路由相关的东西封装到了router.go中，所以这里直接引用router
+type Engine struct {
+	router *router
 }
 
-// New 在使用之前很自然需要先创建一个Engineer对象
-func New() *Engineer {
-	//结构体的初始化
-	var e Engineer = Engineer{
-		route: make(map[string]HandlerFunc, 0),
-	}
-	return &e
+// New is the constructor of gee.Engine
+func New() *Engine {
+	//本来直接创map，但封到router中
+	return &Engine{router: newRouter()}
 }
 
-//ok通过上面的操作我们已经有了一个初始化后的map了，下一笔就需要考虑如何为map添加路由了
-
-// GET 首先当然是经典的Get方法
-// Get方法的输入是路由对应的url以及处理这个路由的方法
-func (e *Engineer) GET(addr string, handler HandlerFunc) {
-	//因为每个Restful风格下每个url对应多种请求类型，所以依据Get以及addr构建key
-	//本来这里可以直接往map中添加数据的，但是为了代码复用单开一个函数处理这个行为
-	e.addRouter("GET", addr, handler)
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
 }
 
-func (e *Engineer) addRouter(method string, addr string, handler HandlerFunc) {
-	key := method + "-" + addr
-	if handler == nil {
-		fmt.Printf("url %s do not have a correct handler", addr)
-	}
-	e.route[key] = handler
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
 }
 
 // POST defines the method to add POST request
-func (e *Engineer) POST(pattern string, handler HandlerFunc) {
-	e.addRouter("POST", pattern, handler)
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
 }
 
-// Run 为了把http包的服务包装起来，需要有一个函数将ListenAndServe封装起来
-// 并且把我们的engineer传入上面那个函数
-// 同时为了处理异常，最好添加一个err的返回值
-func (e *Engineer) Run(addr string) (err error) {
-	return http.ListenAndServe(addr, e)
+// Run defines the method to start a http server
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
 }
 
-// 为了把engineer 传入到ListenAndServe函数中，需要实现方法ServeHTTP
-func (e *Engineer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	//内部的方法应当可以处理所有的请求
-	//所以这里就应该直接查询当前请求对应的handler是哪一个，
-	//然后调用各个handler直接处理请求了
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := e.route[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	//这里可以看出来context是对 w 与 req 的封装
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
