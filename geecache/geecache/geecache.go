1cac7677c630b87debc073822cb37888ba767ff4
whiteeeLi <1521803664@qq.com>
2024-07-16 15:47:51 +0800

    geecache day5 finish

---
+++ geecache/geecache/geecache.go
@@ -0,0 +1,118 @@
+package geecache
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+// A Getter loads data for a key.
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+// A GetterFunc implements Getter with a function.
+type GetterFunc func(key string) ([]byte, error)
+
+// Get implements Getter interface function
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+// A Group is a cache namespace and associated data loaded spread over
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache cache
+	//新增 peers 用于获取分布式节点
+	peers PeerPicker
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+// NewGroup create a new instance of Group
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+// GetGroup returns the named group previously created with NewGroup, or
+// nil if there's no such group.
+func GetGroup(name string) *Group {
+	mu.RLock()
+	g := groups[name]
+	mu.RUnlock()
+	return g
+}
+
+// Get value for a key from cache
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache] hit")
+		return v, nil
+	}
+	//当没有成功获取数据时才会执行下面的代码，尝试从其他地方获取数据
+	return g.load(key)
+}
+
+// 工具函数，服务于load函数，实现从peer中获取数据
+func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
+	bytes, err := peer.Get(g.name, key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	return ByteView{b: bytes}, nil
+}
+
+// 因为添加了peers，所以load函数需要增加从分布式节点获取数据的代码
+func (g *Group) load(key string) (value ByteView, err error) {
+	//新增代码，从分布式节点获取数据
+	if g.peers != nil {
+		if peer, ok := g.peers.PickPeer(key); ok {
+			if value, err = g.getFromPeer(peer, key); err == nil {
+				return value, nil
+			}
+			log.Println("[GeeCache] Failed to get from peer", err)
+		}
+	}
+	//新增代码结束
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
+
+// RegisterPeers registers a PeerPicker for choosing remote peer
+func (g *Group) RegisterPeers(peers PeerPicker) {
+	if g.peers != nil {
+		panic("RegisterPeerPicker called more than once")
+	}
+	g.peers = peers
+}
---
+++ geecache/geecache/lru/lru.go
@@ -0,0 +1,99 @@
+package lru
+
+import "container/list"
+
+type Cache struct {
+	//多考虑了Cache的两个参数maxBytes 是允许使用的最大内存，nbytes 是当前已使用的内存
+	maxBytes int64
+	nbytes   int64
+	// 这里队列使用的是指针，合理，直接使用队列太大了
+	ll *list.List
+	// 使用队列中定义的Element
+	cache map[string]*list.Element
+	//  是某条记录被移除时的回调函数，可以为 nil。
+	OnEvicted func(key string, value Value)
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+// Value use Len to count how many bytes it takes
+type Value interface {
+	Len() int
+}
+
+// New 构造函数
+func New(maxBytes int64, onEvicted func(key string, value Value)) *Cache {
+	result := &Cache{
+		maxBytes:  maxBytes,
+		nbytes:    0,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+	return result
+}
+
+// Get 用于查询
+// 这玩意需要并行么，我们之前实现了一个用于安全接受数据的结构体
+// 所以这里应该把数据写入结构体中并进行返回
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	//如果查询到则执行if语句中的内容
+	if ele, ok := c.cache[key]; ok {
+		//将对应元素放到队尾，这里以Front为队尾
+		c.ll.MoveToFront(ele)
+		//这里其实要和插入的函数相互呼应，插入的时候element中的Value是什么
+		//这里就因该提取出什么
+		//这里有一个类型断言
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	//没查询到直接返回空值
+	return nil, false
+}
+
+// Add 新增
+func (c *Cache) Add(key string, value Value) {
+	//en := entry{key, value}
+	//检查字典，如果已经存在key则进行修改
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		//更新Cache储存的长度数值
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		//没找到的话就是新增
+		//插入队尾
+		ele := c.ll.PushFront(&entry{key, value})
+		//插入map
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+	//当cache容量满了，需要删除队尾的元素保证cache有空的地方
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
+
+// RemoveOldest removes the oldest item
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		//学到了一个新的用法，使用delete删除map中的元素
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+// Len the number of cache entries
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
